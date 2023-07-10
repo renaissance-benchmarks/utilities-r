a1e66b76a73f75c67277a0266a0c505c11d2e9f2
Petr Tuma <petr.tuma@d3s.mff.cuni.cz>
2023-07-10 13:04:41 +0200

    Identifying regular artifacts by interval

--- R/regular.R
+++ R/regular.R
@@ -20,15 +20,16 @@ list_dimensions <- function (.data) {
 #' Identify regular occurrences based on position.
 #'
 #' Identifies regular occurrences in a list of artifact positions and intervals.
-#' An artifact is considered regular if it appears in certain minimum share of runs.
+#' An artifact is considered regular if it appears in the same position
+#' in certain minimum share of runs.
 #'
-#' @param .artifact Artifact positions and intervals.
+#' @param .artifacts Artifact positions and intervals.
 #' @param .dimensions Data dimensions to compute share from.
 #' @param .min_share Minimum share of runs to identify regular artifacts.
 #' @param .min_runs Minimum count of runs to compute share from.
 #' @return Tibble with regular artifacts list.
 #' @export
-list_regular_artifacts <- function (.artifact, .dimensions, .min_share = 2/3, .min_runs = 11) {
+list_regular_artifacts_by_position <- function (.artifacts, .dimensions, .min_share = 2/3, .min_runs = 11) {
 
     counts_artifacts <- .artifacts |>
         group_by (vm, benchmark, index) |>
@@ -36,13 +37,11 @@ list_regular_artifacts <- function (.artifact, .dimensions, .min_share = 2/3, .m
         mutate (runs_delta = 0L)
 
     deltas_runs_first <- .dimensions |>
-        group_by (vm, run, benchmark) |>
-        summarize (index = 1L, runs_delta = +1L, .groups = 'drop') |>
-        select (-run)
+        mutate (index = 1L, runs_delta = +1L) |>
+        select (-run, -index_last, -total_last)
     deltas_runs_last <- .dimensions |>
-        group_by (vm, run, benchmark) |>
-        summarize (index = index_last + 1L, runs_delta = -1L, .groups = 'drop') |>
-        select (-run)
+        mutate (index = index_last + 1L, runs_delta = -1L) |>
+        select (-run, -index_last, -total_last)
 
     timeline <- bind_rows (counts_artifacts, deltas_runs_first, deltas_runs_last) |>
         group_by (vm, benchmark) |>
@@ -54,6 +53,63 @@ list_regular_artifacts <- function (.artifact, .dimensions, .min_share = 2/3, .m
     result <- timeline |>
         mutate (share = artifacts / runs) |>
         filter (share >= .min_share, runs >= .min_runs)
+
+    return (result)
+}
+
+
+#' Identify regular occurrences based on intervals.
+#'
+#' Identifies regular occurrences in a list of artifact positions and intervals.
+#' An artifact is considered regular if it appears in the same interval
+#' in certain minimum share of runs.
+#'
+#' @param .artifacts Artifact positions and intervals.
+#' @param .dimensions Data dimensions to compute share from.
+#' @param .min_share Minimum share of runs to identify regular artifacts.
+#' @param .min_runs Minimum count of runs to compute share from.
+#' @return Tibble with regular artifacts list.
+#' @export
+list_regular_artifacts_by_interval <- function (.artifacts, .dimensions, .min_share = 2/3, .min_runs = 11) {
+
+    deltas_artifacts_before <- .artifacts |>
+        mutate (total = total_before, runs_delta = 0L, artifacts_delta = +1L) |>
+        select (-run, -total_before, -total_after, -index)
+    deltas_artifacts_after <- .artifacts |>
+        mutate (total = total_after, runs_delta = 0L, artifacts_delta = -1L) |>
+        select (-run, -total_before, -total_after, -index)
+
+    deltas_runs_first <- .dimensions |>
+        mutate (total = 0, runs_delta = +1L, artifacts_delta = 0L) |>
+        select (-run, -index_last, -total_last)
+    deltas_runs_last <- .dimensions |>
+        mutate (total = total_last, runs_delta = -1L, artifacts_delta = 0L) |>
+        select (-run, -index_last, -total_last)
+
+    timeline <- bind_rows (deltas_artifacts_before, deltas_artifacts_after, deltas_runs_first, deltas_runs_last) |>
+        group_by (vm, benchmark) |>
+        arrange (total, .by_group = TRUE) |>
+        mutate (runs = cumsum (runs_delta), artifacts = cumsum (artifacts_delta)) |>
+        select (-runs_delta, -artifacts_delta) |>
+        ungroup ()
+
+    result_interim <- timeline |>
+        mutate (share = artifacts / runs) |>
+        group_by (vm, benchmark) |>
+        mutate (current = (share >= .min_share) & (runs >= .min_runs), previous = c (FALSE, head (current, -1)))
+
+    result_before <- result_interim |>
+        filter (current & !previous) |>
+        group_by (total, .add = TRUE) |>
+        summarize (.groups = 'drop') |>
+        rename (total_before = total)
+    result_after <- result_interim |>
+        filter (previous & !current) |>
+        group_by (total, .add = TRUE) |>
+        summarize (.groups = 'drop') |>
+        rename (total_after = total)
+
+    result <- inner_join (result_before, result_after, by = c ('vm', 'benchmark'))
 
     return (result)
 }
--- tests/testthat/test-regular.R
+++ tests/testthat/test-regular.R
@@ -1,4 +1,4 @@
-test_that ('regular artifact listing works on artificial data', {
+test_that ('regular artifact listing by position works on artificial data', {
     test_data <- rren_artificial (c (abs (rnorm (1000, mean = 0)), abs (rnorm (1000, mean = 100))), 2, 22, 2)
 
     test_dimensions <- list_dimensions (test_data)
@@ -10,10 +10,28 @@ test_that ('regular artifact listing works on artificial data', {
     expect_tibble (test_segments, nrow = 88)
     expect_integer (test_segments $ index, 1001 - 8, 1001 + 8)
 
-    test_artifacts <- list_regular_artifacts (test_segments, test_dimensions)
+    test_artifacts <- list_regular_artifacts_by_position (test_segments, test_dimensions)
     expect_tibble (test_artifacts, nrow = 4)
     expect_set_equal (test_artifacts $ index, test_segments $ index)
     expect_integer (test_artifacts $ artifacts, 22L)
     expect_integer (test_artifacts $ runs, 22L)
     expect_numeric (test_artifacts $ share, 1)
+})
+
+test_that ('regular artifact listing by interval works on artificial data', {
+    test_data <- rren_artificial (c (abs (rnorm (1000, mean = 0)), abs (rnorm (1000, mean = 100))), 2, 22, 2)
+
+    test_dimensions <- list_dimensions (test_data)
+    expect_tibble (test_dimensions, nrow = 88)
+    expect_integer (test_dimensions $ index_last, 2000)
+    expect_numeric (test_dimensions $ total_last, max (test_data $ total))
+
+    test_segments <- list_segment_boundaries (test_data, time, 'barrett')
+    expect_tibble (test_segments, nrow = 88)
+    expect_integer (test_segments $ index, 1001 - 8, 1001 + 8)
+
+    test_artifacts <- list_regular_artifacts_by_interval (test_segments, test_dimensions)
+    expect_tibble (test_artifacts, nrow = 4)
+    expect_set_equal (test_artifacts $ total_before, test_segments $ total_before)
+    expect_set_equal (test_artifacts $ total_after, test_segments $ total_after)
 })
